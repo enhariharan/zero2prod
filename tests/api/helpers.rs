ad14d28d83705d19778c1149446db3c13978faa0
Hariharan Narayanan <harihn@simplygrow.in>
2026-06-11 18:05:10 +0530

    Implement basic authentication for newsletter publishing and restrict unauthorized access
    - Add `users` table migrations with `user_id`, `username`, and `password` fields.
    - Implement `basic_authentication` function for extracting and verifying `Authorization` header credentials.
    - Add user validation in `publish_newsletter` route to ensure only authorized users can publish newsletters.
    - Extend test suite with scenarios for missing and valid authorization headers.
    - Update test utilities to insert test users and use basic authentication for newsletter requests.
    - Add `base64` dependency for decoding credentials.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -67,12 +67,22 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        let (username, password) = self.test_user().await;
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
+            .basic_auth(username, Some(password))
             .json(&body)
             .send()
             .await
             .expect("Failed to execute request.")
+    }
+
+    pub async fn test_user(&self) -> (String, String) {
+        let row = sqlx::query!("SELECT username, password FROM users LIMIT 1",)
+            .fetch_one(&self.connection_pool)
+            .await
+            .expect("Failed to create test users.");
+        (row.username, row.password)
     }
 }
 
@@ -121,12 +131,26 @@ pub async fn spawn_app() -> TestApp {
     let spawned_server = tokio::spawn(application.run_until_stopped());
     drop(spawned_server);
 
-    TestApp {
+    let test_app = TestApp {
         address: format!("http://localhost:{}", application_port),
         port: application_port,
         connection_pool: get_connection_pool(&configuration.database),
         email_server,
-    }
+    };
+    add_test_user(&test_app.connection_pool).await;
+    test_app
+}
+
+async fn add_test_user(pool: &PgPool) {
+    sqlx::query!(
+        "INSERT INTO users (user_id, username, password) VALUES ($1, $2, $3)",
+        Uuid::new_v4(),
+        Uuid::new_v4().to_string(),
+        Uuid::new_v4().to_string(),
+    )
+    .execute(pool)
+    .await
+    .expect("Failed to create test users.");
 }
 
 async fn configure_test_database(config: &DatabaseSettings) -> PgPool {
