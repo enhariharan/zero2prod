4f94e344b39f712b20a242faea6cede27dc33cbb
Hariharan Narayanan <harihn@simplygrow.in>
2026-06-12 17:37:20 +0530

    Add basic HTML pages for home and login

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,15 @@
 mod greet;
 mod health_check;
+mod home;
+mod login;
 mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
 
 pub use greet::*;
 pub use health_check::*;
+pub use home::*;
+pub use login::*;
 pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
