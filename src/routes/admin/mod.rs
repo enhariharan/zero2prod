022c52072649681624ca1a0b0f6ab02c8d5d8d31
Hariharan Narayanan <harihn@simplygrow.in>
2026-06-14 09:14:18 +0530

    Remove `/admin/newsletters` route and unused dependencies
    - Delete `/admin/newsletters` modules and corresponding references.
    - Remove unused crates (`hex`, `hmac`, `htmlescape`, `urlencoding`, `sha2`, etc.) from `Cargo.toml` and `Cargo.lock`.
    - Fix a minor typo in `home.html`.

--- src/routes/admin/mod.rs
+++ src/routes/admin/mod.rs
@@ -1,9 +1,7 @@
 mod dashboard;
 mod logout;
-mod newsletters;
 mod password;
 
 pub use dashboard::*;
 pub use logout::*;
-pub use newsletters::*;
 pub use password::*;
--- src/routes/admin/newsletters/mod.rs
+++
@@ -1,3 +0,0 @@
-mod get;
-
-use get::*;
